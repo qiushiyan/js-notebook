36d8a071315c8aeaae2d6856dbd6493451d4b5cc
qiushi <qiushi.yann@gmail.com>
2021-03-30 21:47:36 +0800

    scaffold esbuild plugins

--- readme.md
+++ readme.md
@@ -2,7 +2,26 @@
 
 An interactive notebook for JavaScript
 
-Technologies used
+## Technologies
 
-- React and TypeScript
-- Redux (with Redux Toolkit)
+The app is mainly put together with React and TypeScript. The following tools serve one or two specific purpose.
+
+- **Redux and Redux Toolkit**: state management
+
+- **Redux Thunk**: handle asyncronous logic inside Redux
+
+- **esbuild-wasm**: transpile and bundle user input code
+
+- **Vite**: start local server and build the entire app
+
+## Challenges
+
+### Transpiling and bundling user input
+
+esbuild-wasm enables us to use esbuild inside the browser.
+
+To allow users to import any npm module without haivng to download it locally, for example `import React from "react"` will "just work", we need a way of telling esbuild-wasm to first resolve the repository where these source files are stored, and then fetch the source code.
+
+### Code execution inside the browser
+
+Yser-provided code may be malicious, result in errors, or mutate the dom.
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
-import React, { useState } from "react";
+import React, { FormEvent, useEffect, useState, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetchPlugin";
 
 function App() {
-  return <div className="App">welcome to js-notebook</div>;
+  const [input, setInput] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.11.0/esbuild.wasm",
+    });
+    ref.current = true;
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!ref.current) {
+      return;
+    }
+    const result = await esbuild.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: { global: "window" },
+    });
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div className="App">
+      welcome to js-notebook
+      <form onSubmit={handleSubmit}>
+        <textarea
+          cols={60}
+          rows={10}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <button style={{ display: "block" }}>submit</button>
+      </form>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 export default App;
