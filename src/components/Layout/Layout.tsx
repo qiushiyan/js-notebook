cf1115b90f1ddb2759fd54c3977dc3b08e7a1159
qiushi <qiushi.yann@gmail.com>
2021-04-06 23:18:53 +0800

    remove previous client src folder

--- src/components/Layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "bulmaswatch/superhero/bulmaswatch.min.css";
-
-export const Layout: React.FC = ({ children }) => {
-  return <div className="container">{children}</div>;
-};
-
-export default Layout;
