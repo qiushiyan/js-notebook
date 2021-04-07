2b9420b50d6e6a7f58b4bc87412ad0fcaa53e58d
qiushi <qiushi.yann@gmail.com>
2021-04-07 15:49:06 +0800

    setup cli and local-api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,11 +6,29 @@ interface Options {
   port: string;
 }
 
+const serveAction = async (filename = "notebook.js", { port }: Options) => {
+  const dir = path.join(process.cwd(), path.dirname(filename));
+  try {
+    await serve(parseFloat(port), path.basename(filename), dir);
+    console.log(
+      `Notebook live at http://localhost:${port} \n file: ${path.basename(
+        filename
+      )} \n directory: ${dir}`
+    );
+  } catch (error) {
+    if (error.code === "EADDRINUSE") {
+      console.error(
+        `${port} already in use, try using a different port via the --port option`
+      );
+    } else {
+      console.error("something went wrong, try start the server again");
+    }
+    process.exit(1);
+  }
+};
+
 export const serveCommand = new Command()
   .command("serve [filename]")
   .option("-p, --port <number>", "port to run server on", "3001")
   .description("open a file for editing")
-  .action((filename = "notebook.js", options: Options) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseFloat(options.port), path.basename(filename), dir);
-  });
+  .action(serveAction);
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,18 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log(port, filename, dir);
+  const app = express();
+
+  app.use(
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      ws: true,
+      logLevel: "silent",
+    })
+  );
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
 };
